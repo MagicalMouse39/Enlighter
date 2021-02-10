7ab7082dceabd34b97d183b49b0ffdaed3885ecf
MagicalMouse39 <31729116+MagicalMouse39@users.noreply.github.com>
2021-02-10 20:33:08 +0100

    Update

--- Enlighter/EnlighterRemote/EnlighterUI.cs
+++ Enlighter/EnlighterRemote/EnlighterUI.cs
@@ -9,5 +9,20 @@ namespace EnlighterRemote
 {
     class EnlighterUI : Form
     {
+
+        private EnlightmentUtils engine;
+
+        private void InitializeComponent()
+        {
+            this.engine = new EnlightmentUtils();
+
+            this.SuspendLayout();
+            // 
+            // EnlighterUI
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "EnlighterUI";
+            this.ResumeLayout(false);
+        }
     }
 }
