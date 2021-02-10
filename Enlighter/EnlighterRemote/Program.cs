4d8654d76af7e720cd5465956e1bc06024535236
MagicalMouse39 <31729116+MagicalMouse39@users.noreply.github.com>
2021-02-10 20:22:02 +0100

    Update, gui setup

--- Enlighter/EnlighterRemote/Program.cs
+++ Enlighter/EnlighterRemote/Program.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace EnlighterRemote
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            EnlighterUI ui = new EnlighterUI();
+
+            Application.EnableVisualStyles();
+            Application.Run(ui);
         }
     }
 }
